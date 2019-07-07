e9dc38d92e1b94bbcb5fdf3066acb8c36a3d2f20
Raul Taranu <597630+happyraul@users.noreply.github.com>
2019-07-07 15:53:14 +0200

    add get_item_by

--- auction.ex
+++ auction.ex
@@ -48,4 +48,12 @@ defmodule Auction.FakeRepo do
   def get!(Item, id) do
     Enum.find(@items, fn(item) -> item.id === id end)
   end
+
+  def get_by(Item, attrs) do
+    Enum.find(@items, fn(item) ->
+      Enum.all?(Map.keys(attrs), fn(key) ->
+        Map.get(item, key) === attrs[key]
+      end)
+    end)
+  end
 end
